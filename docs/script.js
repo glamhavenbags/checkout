6f3b58cef97de325da26d7cf67f197260df1b92a
glamhavenbags <info.glamhavenbags@gmail.com>
2024-11-30 12:56:21 +0200

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () { 
+document.addEventListener('DOMContentLoaded', function () {
   const form = document.getElementById('checkout-form');
 
   // استخراج المعلمات من الرابط
